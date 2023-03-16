4c6cb27246b5a16243da91a3c3ee79e50b384149
Bryan Caua Custodio da Silva <103138568+Bryan-Caua@users.noreply.github.com>
2023-03-16 17:30:16 -0300

    projeto-finalizado

---
+++ src/routers/users.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { createUserController } from "../controllers/user/createUserControllers";
+import { listUserController } from "../controllers/user/listUsersController";
+import validationToken from "../middlewares/validationUserToken";
+import ensureDataValidMiddleware from "../middlewares/ensuredata";
+import {
+  userSerializer,
+  userSerializerUpdate,
+} from "../serializer/user.serializer";
+import validationIsAdmin from "../middlewares/validationIsAdmin";
+import updateUserController from "../controllers/user/updateUser";
+import deleteUserController from "../controllers/user/deleteUser";
+import verifyEmailExist from "../middlewares/emailExistMiddlewares";
+
+const userRoutes = Router();
+userRoutes.post(
+  "",
+  ensureDataValidMiddleware(userSerializer),
+  createUserController
+);
+userRoutes.get("", validationToken, validationIsAdmin, listUserController);
+userRoutes.patch(
+  "/:id",
+  ensureDataValidMiddleware(userSerializerUpdate),
+  verifyEmailExist,
+  validationToken,
+  updateUserController
+);
+userRoutes.delete(
+  "/:id",
+  validationToken,
+  validationIsAdmin,
+  deleteUserController
+);
+
+export default userRoutes;
